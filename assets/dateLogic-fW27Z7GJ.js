f2dc759001c03ee6ce8b9d71bc2b23e3441245e6
Lenni009 <guentherhanslphdyt@gmail.com>
2024-03-30 16:52:57 +0000

    Deploying to gh-pages from @ NMSCD/NMSWikiPageCreator@510069e4a7f395b8a8eb622dacf6004fe90771aa 🚀

--- assets/dateLogic-fW27Z7GJ.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./links-395YRII2.js";function s(t){const n=t.value,s=t.dataset.destNoauto;a(n.replaceAll("-","/"),s)}function n(t){const n=(new Date).getFullYear();a(n.toString(),t)}export{n as g,s as p};
