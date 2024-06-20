b78bdba6616fab842b53f3c324582daa44f7e997
Lenni009 <guentherhanslphdyt@gmail.com>
2024-06-20 17:12:20 +0200

    deploy branch on PR

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 /// <reference types="vitest" />
-import { fileURLToPath, URL } from 'node:url';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+const env = loadEnv('', process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: env.VITE_BASE_PATH,
+  // base: '/NMSWikiPageCreator/',
   plugins: [vue()],
   test: {
     browser: {
@@ -17,24 +20,24 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        base: resolve(__dirname, 'base.html'),
-        biofrig: resolve(__dirname, 'biofrig.html'),
-        derelict: resolve(__dirname, 'derelict.html'),
-        fauna: resolve(__dirname, 'fauna.html'),
-        flora: resolve(__dirname, 'flora.html'),
-        mineral: resolve(__dirname, 'mineral.html'),
-        moon: resolve(__dirname, 'moon.html'),
-        multitool: resolve(__dirname, 'multitool.html'),
-        planet: resolve(__dirname, 'planet.html'),
-        racetrack: resolve(__dirname, 'racetrack.html'),
-        sandworm: resolve(__dirname, 'sandworm.html'),
-        settlement: resolve(__dirname, 'settlement.html'),
-        starship: resolve(__dirname, 'starship.html'),
-        system: resolve(__dirname, 'system.html'),
-      },
+      input: [
+        'index.html',
+        'about.html',
+        'base.html',
+        'biofrig.html',
+        'derelict.html',
+        'fauna.html',
+        'flora.html',
+        'mineral.html',
+        'moon.html',
+        'multitool.html',
+        'planet.html',
+        'racetrack.html',
+        'sandworm.html',
+        'settlement.html',
+        'starship.html',
+        'system.html',
+      ],
     },
   },
   resolve: {
