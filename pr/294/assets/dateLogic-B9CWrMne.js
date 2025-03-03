88a828de072d9c619ee5ca77aed77ba82f8a670b
Lenni009 <Lenni009@users.noreply.github.com>
2025-03-03 20:30:11 +0000

    Deploying to gh-pages from @ NMSCD/NMSWikiPageCreator@668610b7fc1b06a72f01371b359af004bd6c273c 🚀

---
+++ pr/294/assets/dateLogic-B9CWrMne.js
@@ -0,0 +1,1 @@
+import{G as a}from"./links-rbsmWE0W.js";function s(t){const s=t.value,n=t.dataset.destNoauto;a(s.replaceAll("-","/"),n)}function n(t){const s=(new Date).getFullYear();a(s.toString(),t)}export{n as g,s as p};
